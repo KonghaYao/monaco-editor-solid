2e124282a704bdeaf25fbcd7c5c862f70c51dcf5
3446798488 <3446798488@qq.com>
2024-01-09 09:16:54 +0000

    build(deps): 调整打包方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,38 @@
 import solid from "vite-plugin-solid";
 import svgLoader from 'vite-svg-loader'
 import { defineConfig } from "vite";
+import p from './package.json'
+export default defineConfig(({ mode }) => {
+    console.log(mode)
+    return {
+        plugins: [
 
-export default defineConfig({
-    plugins: [
-        // wasm()
-        solid(),
-        svgLoader({
-            defaultImport: 'component' // or 'raw'
-          }),
-        {
-            name: "232322",
-            resolveId: {
-                order: "pre",
-                handler(source, importer, options) {
-                    if (source.endsWith('.wasm')) {
-                        return { id: source + '?url' }
+            solid(),
+            svgLoader({
+                defaultImport: 'component' // or 'raw'
+            }),
+            {
+                name: "232322",
+                resolveId: {
+                    order: "pre",
+                    handler(source, importer, options) {
+                        if (mode === 'production' && Object.keys({ ...p.dependencies, ...p.devDependencies, ...p.peerDependencies }).filter(i =>
+                            !['@vscode/codicons','vscode-icons-js'].includes(i)
+                        ).some(i => source.startsWith(i))) return { id: source, external: true }
+                        if (source.endsWith('.wasm')) {
+                            return { id: source + '?url' }
+                        }
                     }
-                }
+                },
             },
+        ],
+        build: {
+            lib: {
+                entry: "./src/index",
+                formats: ['es']
+            },
+            target: "esnext"
         }
-    ],
-    build: {
-        target: "esnext"
+
     }
 });
